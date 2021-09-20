f9511c5dde520c9fa30c1c41a7e2c5d90ca20323
Mansoor Nasir <mrmansoornasir@gmail.com>
2021-09-21 01:31:11 +0500

    sd

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   bracketSpacing: true,
   jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
-  endOfLine: 'auto',
+  singleQuote: false,
+  trailingComma: "all",
+  endOfLine: "auto",
   // Override any other rules you want
 };
