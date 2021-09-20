09e58146a55267f33c772f3614526ed766452c3f
Mansoor Nasir <mrmansoornasir@gmail.com>
2021-09-20 23:03:26 +0500

    boiler

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+  // Override any other rules you want
+};
