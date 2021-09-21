7be08d22878772c6bc1b272b630dd8838b6e897d
Mansoor Nasir <mrmansoornasir@gmail.com>
2021-09-21 17:28:07 +0500

    ttt

---
+++ src/App-copy.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Boilerplate React Application</h1>
+      <h1>Hello Fawad :D</h1>
+      <h1>Hello Test :D</h1>
+    </div>
+  );
+}
+
+export default App;
